b20eb0e183c2951e81493e860627226ae32b343d
MarCassMari <marcassmachado32@gmail.com>
2025-09-01 13:07:29 -0300

    feat: alterando porta e acrescimo app use express

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ dotenv.config()
 import { PostgresHelper } from './src/db/postgres/helper.js'
 
 const app = express()
+app.use(express.json())
 
 app.get('/', async (req, res) => {
     const result = await PostgresHelper.query('SELECT NOW()')
     return res.send(JSON.stringify(result.rows))
 })
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on http://localhost:8000')
 })
