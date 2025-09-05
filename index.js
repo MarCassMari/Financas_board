c20bc2a06dc05ce76f3833f732642cc9bddcc88a
MarCassMari <marcassmachado32@gmail.com>
2025-09-05 15:21:08 -0300

    feat: add create user controller

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.post('/api/users', async (req, res) => {
+    const createUserController = new CreateUserController()
+
+    const { statusCode, body } = await createUserController.execute(req)
+    res.status(statusCode).send(body)
+})
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on http://localhost:8000')
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
