4bcb0fda7ff8432bfb3b16f4336ef465aac8217b
MarCassMari <marcassmachado32@gmail.com>
2025-09-05 11:16:51 -0300

    feat: add postgres create user repository

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 import dotenv from 'dotenv'
 import express from 'express'
 dotenv.config()
-import { PostgresHelper } from './src/db/postgres/helper.js'
 
 const app = express()
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-    const result = await PostgresHelper.query('SELECT NOW()')
-    return res.send(JSON.stringify(result.rows))
-})
 app.listen(process.env.PORT, () => {
     console.log('Server is running on http://localhost:8000')
 })
